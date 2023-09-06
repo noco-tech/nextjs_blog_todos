390db809c52b0d01ae63c54f2a4e7f4a32d27ffa
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-06 21:02:19 +0900

    リファクタリング

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -54,7 +54,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const { post: post } = await getPostData(params.id);
+  const post = await getPostData(params.id);
   return {
     props: {
       post,
