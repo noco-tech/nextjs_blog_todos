fb3e764dbbb8bf334f7a88de32942c251f127d20
sadainoaraisan <73414778+sadainoaraisan@users.noreply.github.com>
2023-09-06 14:40:23 +0900

    useSWR, taskページ

---
+++ components/Task.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+
+export const Task = ({task}) => {
+  return (
+    <div>
+      <span>{task.id}</span>
+      {" : "}
+      <Link href={`/tasks/${task.id}`}>
+        <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
+          {task.title}
+        </span>
+      </Link>
+    </div>
+  );
+}
+
+
--- pages/task-page.js
+++ pages/task-page.js
@@ -1,30 +1,67 @@
 import Link from "next/link";
 import { Layout } from "../components/Layout";
+import { getAllTasksData } from "../lib/tasks";
+import { Task } from "../components/Task";
+import useSWR from "swr";
+import { useEffect } from "react";
 
-const TaskPage = ({}) => {
+//useSWRを使う場合
+const fetcher = (url) => fetch(url).then((res) => res.json());
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
+
+const TaskPage = ({ staticfilteredTasks }) => {
+  //useSWRを使う場合
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    fallbackData: staticfilteredTasks,
+  });
+  const filteredTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+
+  useEffect(() => {
+    mutate();
+  }, [])
+
   return (
-    <Layout title="Task page">
-      <Link href="/main-page">
-      <div className="flex flex-col items-center justify-center flex-1 text-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 mr-3"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
-          />
-        </svg>
-        <span className="text-xl">Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <>
+      <Layout title="Task page">
+        <p className="text-4xl mb-7">Task page</p>
+        <ul>
+          {filteredTasks &&
+            filteredTasks.map((task) => <Task key={task.id} task={task} />)}
+        </ul>
+
+        <Link href="/main-page">
+          <div className="flex mt-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 mr-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+              />
+            </svg>
+            <span className="text-xl">Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </>
   );
 };
 
 export default TaskPage;
+
+export async function getStaticProps() {
+  const staticfilteredTasks = await getAllTasksData();
+
+  return {
+    props: { staticfilteredTasks },
+    revalidate: 3,
+  };
+}
